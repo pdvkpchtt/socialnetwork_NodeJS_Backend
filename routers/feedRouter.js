c19da90de193d6c50d738a00ac3866ed9e089784
Danil <kabirdanilov14@gmail.com>
2023-10-23 13:43:57 +0500

    added deleting posts

--- routers/feedRouter.js
+++ routers/feedRouter.js
@@ -5,20 +5,9 @@ const pool = require("../db");
 router.route("/createpost").post(async (req, res) => {
   const postId = new Date().valueOf();
   const post = await pool.query(
-    "INSERT INTO posts(id, title, text, user_id, category_id) values($1,$2,$3,$4,$5)",
-    [
-      postId,
-      req.body.data.headState,
-      req.body.data.textState,
-      req.body.userId,
-      req.body.data.dropDownState.id,
-    ]
+    "INSERT INTO posts(id, title, text, user_id) values($1,$2,$3,$4)",
+    [postId, req.body.data.headState, req.body.data.textState, req.body.userId]
   );
-});
-
-router.route("/getcategories").post(async (req, res) => {
-  const categories = await pool.query("SELECT * from category");
-  res.json({ data: categories.rows });
 });
 
 router.route("/getallposts").post(async (req, res) => {
@@ -26,8 +15,16 @@ router.route("/getallposts").post(async (req, res) => {
     "select posts.*, users.name from posts join users on users.id = posts.user_id ORDER BY posts.id DESC LIMIT 8 OFFSET $1",
     [req.body.cursor]
   );
-  console.log(posts.rows);
+  // console.log(posts.rows);
   res.send({ data: posts.rows });
+});
+
+module.exports = router;
+
+router.route("/deletepost").post(async (req, res) => {
+  const posts = await pool.query("delete from posts where posts.id = $1", [
+    req.body.postId,
+  ]);
 });
 
 module.exports = router;
