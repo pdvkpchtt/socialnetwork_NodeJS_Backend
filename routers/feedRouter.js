9f0a7300804ac3aa39b2576cf1603fa224ab913d
Danil <kabirdanilov14@gmail.com>
2023-10-23 14:20:59 +0500

    started creating reactions

--- routers/feedRouter.js
+++ routers/feedRouter.js
@@ -19,12 +19,33 @@ router.route("/getallposts").post(async (req, res) => {
   res.send({ data: posts.rows });
 });
 
-module.exports = router;
-
 router.route("/deletepost").post(async (req, res) => {
   const posts = await pool.query("delete from posts where posts.id = $1", [
     req.body.postId,
   ]);
+});
+
+router.route("/getreactions").post(async (req, res) => {
+  const posts = await pool.query("select * from reactions where post_id = $1", [
+    req.body.postId,
+  ]);
+
+  res.send({ data: posts.rows });
+});
+
+router.route("/deletereaction").post(async (req, res) => {
+  const posts = await pool.query("delet from reactions where user_id = $1", [
+    req.body.userId,
+  ]);
+});
+
+router.route("/createreaction").post(async (req, res) => {
+  console.log(req.body);
+  const reactionId = new Date().valueOf();
+  const posts = await pool.query(
+    "INSERT INTO reactions(id, type, user_id, post_id) values($1,$2,$3,$4)",
+    [reactionId, req.body.type, req.body.userId, req.body.postId]
+  );
 });
 
 module.exports = router;
