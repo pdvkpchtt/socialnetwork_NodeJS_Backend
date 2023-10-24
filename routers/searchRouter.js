cce960d4a819d31ad12a1d3bfd881f8af7e123f5
Danil <kabirdanilov14@gmail.com>
2023-10-25 00:15:33 +0500

    done with search

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const pool = require("../db");
 
 router.route("/getusers").post(async (req, res) => {
-  const users = await pool.query("select * from users where name like '%$1%'", [
-    req.body.input,
-  ]);
+  const users = await pool.query(
+    "select * from users where name like $2 ORDER BY id DESC LIMIT 8 OFFSET $1",
+    [req.body.cursor, `%${req.body.input}%`]
+  );
+
+  res.send({ data: users.rows });
 });
 
 module.exports = router;
