16aeceb8286cc7393a864fecb5d418f022261cb2
Danil <kabirdanilov14@gmail.com>
2023-10-25 20:11:59 +0500

    small fix

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -4,8 +4,8 @@ const pool = require("../db");
 
 router.route("/getusers").post(async (req, res) => {
   const users = await pool.query(
-    "select * from users where name like $2 ORDER BY id DESC LIMIT 10 OFFSET $1",
-    [req.body.cursor, `%${req.body.input}%`]
+    "select * from users ORDER BY id DESC LIMIT 10 OFFSET $1",
+    [req.body.cursor]
   );
 
   res.send({ data: users.rows });
